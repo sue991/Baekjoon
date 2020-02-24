9fd9611924e49513ef51f2549caf2313224000a2
sue991 <sue991@naver.com>
2020-02-24 17:07:09 +0900

    200224

---
+++ 15652.py
@@ -0,0 +1,25 @@
+N,M = map(int, input().split())
+
+num_list = [i+1 for i in range(N)]
+check_list = [False]*N
+
+arr = []
+
+def dfs(k):
+    if k == M:
+        print(*arr)
+        return
+
+    for i in range(N):
+        if check_list[i]:
+            continue
+
+        arr.append(num_list[i])
+        dfs(k+1)
+        check_list[i] = True
+        arr.pop()
+
+        for j in range(i+1,N):
+            check_list[j]=False
+
+dfs(0)
