e45833c0d10e984eeba76951d75a9c7360844aaa
sue991 <sue991@naver.com>
2020-04-07 17:48:47 +0900

    200407

---
+++ 1904.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[1000001]= {0};
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for(int i=3;i<n+1;i++){
+        arr[i] = (arr[i-1]+arr[i-2])%15746;
+    }
+
+    printf("%d",arr[n]);
+
+}
