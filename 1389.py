2665e58eb74550f5df5d7a7bc2fbf577dd4e5097
sue991 <sue991@naver.com>
2020-07-08 14:49:20 +0900

    200708

---
+++ 1389.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+N,M = map(int, input().split())
+
+relationship = [[] for _ in range(N)]
+bacon, q = [], deque()
+
+def bfs(k):
+    q.append(k)
+    chk = [-1 for _ in range(N)]
+    chk[k] = 0
+    while q:
+        k = q.popleft()
+        for x in relationship[k]:
+            if chk[x] == -1:
+                chk[x] = chk[k] + 1
+                q.append(x)
+    n = 0
+    for x in chk:
+        if x != -1:
+            n += x
+    return n
+
+for _ in range(M):
+    A,B = map(int, input().split())
+    A -= 1 ; B -= 1 
+    relationship[A].append(B)
+    relationship[B].append(A)
+
+for i in range(N):
+    bacon.append(bfs(i))
+
+ans =[]
+for i in range(N):
+    if min(bacon) == bacon[i]:
+        ans.append(i)
+
+print(min(ans)+1)
