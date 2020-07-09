e731065099c3c32af9ab65de903436d07737c397
sue991 <sue991@naver.com>
2020-07-09 14:11:32 +0900

    200709

--- 2638.py
+++ 2638.py
@@ -11,38 +11,34 @@ for _ in range(N):
 q = deque()
 def bfs(y,x):
     visited= [[0 for _ in range(M)] for _ in range(N)]
-    n = 0
     q.append((y,x))
     dx = [1,-1,0,0]
     dy = [0,0,1,-1]
     while q:
         y,x = q.popleft()
-        if visited[y][x] == 0:
-            visited[y][x] = 1
+        if cheese[y][x] != 0:
+            cheese[y][x] += 1
+            continue
+
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             if 0 <= nx < M and 0 <= ny < N and visited[ny][nx] == 0:
-                if cheese[ny][nx] == 1:
-                    cheese[ny][nx] = 0
-                    n += 1
-                else:
-                    q.append((ny,nx))
-                visited[ny][nx] = 1
-    return n
-                
-# for x in cheese:
-#     if 1 in x:
-#         hours += 1
-#         bfs(0,0)
-
-bfs(0,0)
-print()
-for i in cheese:
-    for j in i:
-        print(j,end=" ")
-    print()
-
-# print(hours)
-
-
+                if cheese[ny][nx] == 0:
+                    visited[ny][nx] = 1
+                q.append((ny,nx))
+    
+    for i in range(N):
+        for j in range(M):
+            if cheese[i][j] >= 3:
+                cheese[i][j] = 0
+            elif cheese[i][j] == 1 or cheese[i][j] == 2:
+                cheese[i][j] = 1
+while True:
+    for i in range(N):
+        for j in range(M):
+            if cheese[i][j] != 0:
+                hours += 1
+                bfs(0,0)
+    break    
+print(hours)
