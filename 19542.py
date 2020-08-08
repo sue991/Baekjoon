1327fa6dbafb1050ba649d3a67b42dc78cd765b7
sue991 <sue991@naver.com>
2020-08-08 14:03:31 +0900

    200808

--- 19542.py
+++ 19542.py
@@ -1,5 +1,13 @@
-
 N,S,D = map(int, input().split())
+relationship = [[] for _ in range(N)]
+
+def dfs(x,l):
+    
+
+
 
 for _ in range(N):
     x,y = map(int, input().split())
+    x -= 1 ; y -= 1
+    relationship[x].append(y)
+    relationship[y].append(x)
