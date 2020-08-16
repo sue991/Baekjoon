8b46aa534571a99bac7431972f08ca8a78688de5
sue991 <sue991@naver.com>
2020-08-16 12:43:19 +0900

    200816

---
+++ 1495.py
@@ -0,0 +1,23 @@
+n, s, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+dp1 = [0 for i in range(m + 1)]
+dp2 = [0 for i in range(m + 1)]
+
+dp1[s] = 1
+for v in lst:
+    for i in range(m + 1):
+        if dp1[i]:
+            if i + v <= m:
+                dp2[i + v] = 1
+            if i - v >= 0:
+                dp2[i - v] = 1
+    dp1 = dp2
+    dp2 = [0] * (m + 1)
+
+ans = -1
+for i in range(m, -1, -1):
+    if dp1[i]:
+        ans = i
+        break
+print(ans)
