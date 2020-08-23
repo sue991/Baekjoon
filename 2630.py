4d72b65e108e7ef5add9b8178e5360cbcaad3c7b
sue991 <sue991@naver.com>
2020-08-23 19:08:18 +0900

    200823

--- 2630.py
+++ 2630.py
@@ -1,3 +1,26 @@
 N = int(input())
 paper = [list(map(int, input().split())) for _ in range(N)]
+white, blue = 0, 0
 
+def div(x,y,n):
+    global white, blue
+    chk = paper[x][y]
+    tmp = n//2
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if chk != paper[i][j]:
+                div(x,y,tmp) # 1
+                div(x,y+tmp,tmp) # 2
+                div(x+tmp,y,tmp) # 3
+                div(x+tmp,y+tmp,tmp) # 4
+                return
+    if chk == 0:
+        white += 1
+    else:
+        blue += 1
+    return
+
+if __name__ == "__main__":
+    div(0,0,N)
+    print(white)
+    print(blue)
