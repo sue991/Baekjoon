a010f456299b666bf05e86f06c015cfb14a3eb83
sue991 <sue991@naver.com>
2022-08-08 15:42:45 +0900

    알바생 강호

---
+++ Greedy/1758.py
@@ -0,0 +1,8 @@
+n = int(input())
+tip = sorted([int(input()) for _ in range(n)], reverse=True)
+
+ans = 0
+for i, money in enumerate(tip):
+    ans += money - i if money - i > 0 else 0
+
+print(ans)
